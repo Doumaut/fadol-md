2a556c46fc0a79759a4f437946be7d1cdf94480e
Doumaut <108480751+Doumaut@users.noreply.github.com>
2022-07-30 21:40:19 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -24,23 +24,23 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "Putricantik Bot✔︎" //namabot kalian
-global.ownername= "Ballsstore☕︎" //nama kalian
+global.botname = "Faruqq bott" //namabot kalian
+global.ownername= "Faruq store" //nama kalian
 global.myweb ="https://Wa.me/6281233083172" //bebas asal jan hapus
 global.youtube = "https://youtube.com/shorts/7ySw5JMgNIo?feature=share" //bebas asal jan hapus
 global.github = "https://github.com/Doumaut/Marogol-" //bebas
-global.email = "sadboysabi366@gmail.com" //bebas
+global.email = "f0556915@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['6281233083172'] //premium user
-global.owner = ["6281233083172","6281246124174"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "6281233083172" // nomor wa kalian
-global.ownernomerr = "+6281233083172" //nmr wa kalian
+global.premium = ['6285738861620'] //premium user
+global.owner = ["6285738861620","6285738861620"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6285738861620" // nomor wa kalian
+global.ownernomerr = "+6285738861620" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.packname = '© Putricantik Bot✔︎' //sticker wm ubah
-global.author = 'Di Buat Oleh Ballsstore☕︎' //sticker wm ganti nama kalian
+global.packname = '© Bot adit' //sticker wm ubah
+global.author = 'Di Buat Oleh Adit kang spam' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 
